d1939a201a6832b487d5ff9a55b0f69419f3f041
Ramses di Perna <15826@ma-web.nl>
2015-05-18 18:12:59 +0200

    Vector converter in development with getArrayloopindex function PS still not functional

--- Assets/_Scripts/Movement/MoveToTouch.cs
+++ Assets/_Scripts/Movement/MoveToTouch.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MoveToTouch : MoveableNetworkEntity {
 	
 	private Vector2 _destination = Vector2.zero;
+	private Vector2 _direction = Vector2.zero;
+
+	private bool _controlling = false;
 
 	protected override void Start ()
 	{
@@ -17,20 +20,25 @@ public class MoveToTouch : MoveableNetworkEntity {
 
 		if (Input.GetMouseButton (0)) {
 			_destination = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			_objectSpeed = _speed;
+			_direction = _destination - new Vector2(_rigidBody.transform.position.x, _rigidBody.transform.position.y);
+
+			if(_direction.magnitude > _speed / 2 || _objectSpeed <= 0 && _direction.magnitude > 0.6f){
+				_objectSpeed = _speed;
+			}
 		}
 
 		if (_objectSpeed > 0) {
 			Movement();
 			_objectSpeed -= 0.05f; //hoe hard moet het remmen.(des de hoger de value des de sneller dat het object remt).
 		}
+		Debug.Log (VectorConverter.GetRotationSyncVector (Vector2.right,90));
 	}
 
 	private void Movement(){
 
-		Vector2 direction = _destination - new Vector2(transform.position.x, transform.position.y);
-		direction.Normalize ();
-		direction *= _objectSpeed;
-		_rigidBody.transform.position += new Vector3(direction.x,direction.y,0) * Time.deltaTime;
+		_direction.Normalize ();
+		_direction *= _objectSpeed;
+		_rigidBody.transform.position += new Vector3 (_direction.x, _direction.y, 0) * Time.deltaTime;
+
 	}
 }
---
+++ Assets/_Scripts/Utils/VectorConverter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorConverter {
+	public static Vector2 GetRotationSyncVector(Vector2 vectorDirection, float rotationInDegrees){
+
+		Vector2[] _vectorList = new Vector2[]{new Vector2(0,Mathf.Abs(vectorDirection.y)),
+											  new Vector2(Mathf.Abs(vectorDirection.x),0),
+											  new Vector2(0,-Mathf.Abs(vectorDirection.y)),
+											  new Vector2(-Mathf.Abs(vectorDirection.x),0)};
+		Vector2 calclatedVector = Vector2.zero;
+
+		switch(rotationInDegrees.ToString()){
+			case "0":
+				calclatedVector = vectorDirection;
+				break;
+			case "90":
+				calclatedVector = _vectorList[GetArrayLoopIndex(_vectorList,1,System.Array.IndexOf(_vectorList,vectorDirection))];
+				break;
+			case "180":
+				calclatedVector = -vectorDirection;
+				break;
+			case "270":
+				calclatedVector = _vectorList[GetArrayLoopIndex(_vectorList,-1,System.Array.IndexOf(_vectorList,vectorDirection))];
+				break;
+		}
+		return calclatedVector;
+	}
+
+	private static int GetArrayLoopIndex(System.Array array,int amount,int startInArray = 0){
+		int returnIndex = startInArray;
+		returnIndex = amount + startInArray;
+
+		while(returnIndex > array.Length){
+			returnIndex -= array.Length;
+		}
+		while(returnIndex < 0){
+			returnIndex += array.Length;
+		}
+		//Debug.Log (returnIndex);
+		return returnIndex;
+	}
+}
