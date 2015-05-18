4f5adcd643de96be676787f8557dda02a9b89543
Ramses di Perna <15826@ma-web.nl>
2015-05-18 14:35:59 +0200

    mergin

--- Assets/_Scripts/Movement/MoveToTouch.cs
+++ Assets/_Scripts/Movement/MoveToTouch.cs
@@ -23,15 +23,15 @@ public class MoveToTouch : MoveableNetworkEntity {
 
 		if (_objectSpeed > 0) {
 			Movement();
-			_objectSpeed -= 0.05f; //hoe hard moet het remmen.
+			_objectSpeed -= 0.05f; //hoe hard moet het remmen.(des de hoger de value des de sneller dat het object remt).
 		}
 	}
 
 	private void Movement(){
+
 		Vector2 direction = _destination - new Vector2(transform.position.x, transform.position.y);
 		direction.Normalize ();
 		direction *= _objectSpeed;
 		_rigidBody.transform.position += new Vector3(direction.x,direction.y,0) * Time.deltaTime;
-
 	}
 }
