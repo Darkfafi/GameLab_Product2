d6e540b0c7aeb6ee2a2e6accb1831322596db708
Ramses di Perna <15826@ma-web.nl>
2015-05-18 14:29:57 +0200

    Redirected classes and made MoveToTouch class

---
+++ Assets/_Scripts/Entities/MoveableNetworkEntity.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableNetworkEntity : MonoBehaviour {
+	protected float _speed;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncStartEuler = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+	private Vector3 syncEndEuler = Vector3.zero;
+	protected Rigidbody2D _rigidBody;
+	private NetworkView _networkView;
+	private Animator _animator;
+	
+	protected virtual void Awake()
+	{
+		_networkView = this.GetComponent<NetworkView>();
+		_rigidBody = this.GetComponent<Rigidbody2D>();
+		//_animator = this.GetComponent<Animator>();
+	}
+	private void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+		Vector3 syncEuler = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = _rigidBody.position;
+			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = _rigidBody.velocity;
+			stream.Serialize(ref syncVelocity);
+			
+			syncEuler = transform.eulerAngles;
+			stream.Serialize(ref syncEuler);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+			stream.Serialize(ref syncEuler);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = _rigidBody.position;
+			
+			syncEndEuler = syncEuler * syncDelay;
+			syncStartEuler = transform.eulerAngles;
+		}
+	}
+	// Use this for initialization
+	protected virtual void Start () {
+		
+	}
+	
+	protected virtual void Update()
+	{
+		if (_networkView.isMine)
+		{
+			MovementInput();
+		}
+		else
+		{
+			SyncedMovement();
+		}
+	}
+	protected void MovementInput()
+	{
+		
+	}
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		_rigidBody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+		
+		transform.eulerAngles = Vector3.Slerp(syncStartEuler, syncEndEuler, syncTime / syncDelay);
+	}
+
+
+}
--- Assets/_Scripts/Movement/MoveToTouch.cs
+++ Assets/_Scripts/Movement/MoveToTouch.cs
@@ -1,82 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveToTouch : MonoBehaviour {
-	private float lastSynchronizationTime = 0f;
-	private float syncDelay = 0f;
-	private float syncTime = 0f;
-	private Vector3 syncStartPosition = Vector3.zero;
-	private Vector3 syncStartEuler = Vector3.zero;
-	private Vector3 syncEndPosition = Vector3.zero;
-	private Vector3 syncEndEuler = Vector3.zero;
-	private Rigidbody _rigidBody;
-	private NetworkView _networkView;
-	private Animator _animator;
-
-	private void Awake()
-	{
-		_networkView = this.GetComponent<NetworkView>();
-		_rigidBody = this.GetComponent<Rigidbody>();
-		//_animator = this.GetComponent<Animator>();
-	}
-	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
-	{
-		Vector3 syncPosition = Vector3.zero;
-		Vector3 syncVelocity = Vector3.zero;
-		Vector3 syncEuler = Vector3.zero;
-		if (stream.isWriting)
-		{
-			syncPosition = _rigidBody.position;
-			stream.Serialize(ref syncPosition);
-			
-			syncVelocity = _rigidBody.velocity;
-			stream.Serialize(ref syncVelocity);
-			
-			syncEuler = transform.eulerAngles;
-			stream.Serialize(ref syncEuler);
-		}
-		else
-		{
-			stream.Serialize(ref syncPosition);
-			stream.Serialize(ref syncVelocity);
-			stream.Serialize(ref syncEuler);
-			
-			syncTime = 0f;
-			syncDelay = Time.time - lastSynchronizationTime;
-			lastSynchronizationTime = Time.time;
-			
-			syncEndPosition = syncPosition + syncVelocity * syncDelay;
-			syncStartPosition = _rigidBody.position;
-
-			syncEndEuler = syncEuler * syncDelay;
-			syncStartEuler = transform.eulerAngles;
-		}
-	}
-	// Use this for initialization
-	void Start () {
+public class MoveToTouch : MoveableNetworkEntity {
 	
-	}
-	
-	void Update()
-	{
-		if (_networkView.isMine)
-		{
-			MovementInput();
-		}
-		else
-		{
-			SyncedMovement();
-		}
-	}
-	private void MovementInput()
-	{
+	private Vector2 _destination = Vector2.zero;
+	private float _objectSpeed = 0;
 
-	}
-	private void SyncedMovement()
+	protected override void Start ()
 	{
-		syncTime += Time.deltaTime;
-		_rigidBody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
-		
-		transform.eulerAngles = Vector3.Slerp(syncStartEuler, syncEndEuler, syncTime / syncDelay);
+		base.Start ();
+		_speed = 3;
+	}
+
+	protected override void Update ()
+	{
+		base.Update ();
+
+		if (Input.GetMouseButton (0)) {
+			_destination = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			_objectSpeed = _speed;
+		}
+
+		if (_objectSpeed > 0) {
+			Movement();
+			_objectSpeed -= 0.05f; //hoe hard moet het remmen.
+		}
+	}
+
+	private void Movement(){
+		Vector2 direction = _destination - new Vector2(transform.position.x, transform.position.y);
+		direction.Normalize ();
+		direction *= _objectSpeed;
+		_rigidBody.transform.position += new Vector3(direction.x,direction.y,0) * Time.deltaTime;
+
 	}
 }
