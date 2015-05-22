14a3e2e29dd6cb6ba00c38852fbb06ba0cd733bd
menno <run_jongejan@hotmail.com>
2015-05-22 11:45:25 +0200

    Updating Menu to UI instead of GUI

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	private ConnectionHandler _connectionHandler;
+	private UserInfo _userInfo;
+	// Use this for initialization
+	void Awake () {
+		GameObject connector = GameObject.FindGameObjectWithTag(Tags.Connector);
+		_connectionHandler = connector.GetComponent<ConnectionHandler>();
+		_userInfo = connector.GetComponent<UserInfo>();
+	}
+	
+	public void SaveUsername(string username)
+	{
+		_userInfo.username = username;
+	}
+}
